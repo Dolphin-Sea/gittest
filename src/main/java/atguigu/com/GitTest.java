4aa233bf8899340598f18e127f8fbba36a0f6679
Capricornus <Capricornus@atguigu.com>
2021-06-06 09:51:51 +0800

    push commit

--- src/main/java/atguigu/com/GitTest.java
+++ src/main/java/atguigu/com/GitTest.java
@@ -5,7 +5,8 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello master test");
-
         System.out.println("hello hot-fix test");
+        System.out.println("push test");
+
     }
 }
