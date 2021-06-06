62d46a9132e5685fdd5f56fba06b8689a40b603c
Capricornus <Capricornus@atguigu.com>
2021-06-06 10:29:16 +0800

    Gitee commit-push test

--- src/main/java/atguigu/com/GitTest.java
+++ src/main/java/atguigu/com/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("Github push test");
+        System.out.println("Github push test success");
+        System.out.println("Gitee push test");
     }
 }
